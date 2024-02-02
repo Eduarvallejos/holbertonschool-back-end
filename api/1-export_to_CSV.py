7b60e2d581d485c0b9f2dd5aca402f335198596e
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-02-01 22:08:41 -0500

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,22 +2,23 @@
 """We consume APIs to extract fictitious information."""
 import requests
 from sys import argv
+import csv
 
 
 def api_data():
     """We query the name and tasks of an employee."""
     if len(argv) > 1 and argv[1].isdigit():
-        user_ID = int(argv[1])
+        USER_ID = int(argv[1])
 
     else:
         return
 
     """Build the API URL to get user tasks."""
-    api_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_ID}"
+    api_url = f"https://jsonplaceholder.typicode.com/todos?userId={USER_ID}"
     response = requests.get(api_url)
 
     """Build the API URL to get user information"""
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_ID}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{USER_ID}"
     users = requests.get(user_url)
 
     """Check if the user's information request was successful."""
@@ -37,13 +38,25 @@ def api_data():
     else:
         print("Error: Unable to get tasks. Please enter an existing user ID.")
 
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
-          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    """CSV file creation"""
+    csv_file_name = f"{USER_ID}.csv"
+    with open(csv_file_name, mode='w', newline='') as csv_file:
+        # Define the field names (column headers) for the CSV
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
-    # Print titles of completed tasks
-    for todo in todos:
-        if todo["completed"]:
-            print("\t {}".format(todo["title"]))
+        # Create a CSV writer object
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+
+        # Write task data to the CSV file
+        for todo in todos:
+            writer.writerow({
+                "USER_ID": USER_ID,
+                "USERNAME": user["username"],
+                "TASK_COMPLETED_STATUS": todo["completed"],
+                "TASK_TITLE": todo["title"]
+            })
 
 
 if __name__ == "__main__":
